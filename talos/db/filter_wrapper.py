242f887124cbf9ed5de55cf0c187177ee7bde66c
Roy <576477085@qq.com>
2017-11-29 21:24:37 +0800

    修改：优化crud filter逻辑

--- talos/db/filter_wrapper.py
+++ talos/db/filter_wrapper.py
@@ -1,21 +1,73 @@
 # coding=utf-8
 """
-fitcloud.db.crud
-~~~~~~~~~~~~~~~
+talos.db.filter_wrapper
+~~~~~~~~~~~~~~~~~~~~~~~
 
-本模块提供DB的CRUD封装
+本模块提供DB的filter过滤封装
 
 """
 
 from __future__ import absolute_import
 
 
-from fitcloud.core import utils
+from talos.core import utils
 from sqlalchemy.sql.expression import BinaryExpression
 from sqlalchemy.sql.sqltypes import _type_map
 
 
-def cast_from_python_value(column, value):
+def merge(filters, filters_to_merge):
+    """
+    将filters_to_merage合并到filters中
+
+    :param filters: 待合并的filters(注：内容会被修改并返回)
+    :type filters: dict
+    :param filters_to_merge: 待合并的filters(注：内容会被修改)
+    :type filters_to_merge: dict
+    """
+    keys = set(filters.keys()) & set(filters_to_merge.keys())
+    for key in keys:
+        val = filters[key]
+        other_val = filters_to_merge.pop(key)
+        if isinstance(val, dict) and isinstance(other_val, dict):
+            val.update(other_val)
+        elif isinstance(val, dict) and not isinstance(other_val, dict):
+            if utils.is_list_type(other_val):
+                val['in'] = other_val
+            else:
+                val['eq'] = other_val
+        elif not isinstance(val, dict) and isinstance(other_val, dict):
+            if utils.is_list_type(val):
+                other_val['in'] = val
+            else:
+                other_val['eq'] = val
+            filters[key] = other_val
+        else:
+            nval = {}
+            if utils.is_list_type(val):
+                nval['in'] = val
+            else:
+                nval['eq'] = val
+            if utils.is_list_type(other_val):
+                nval['in'] = other_val
+            else:
+                nval['eq'] = other_val
+            filters[key] = nval
+    filters.update(filters_to_merge)
+    return filters
+
+
+def column_from_expression(table, expression):
+    if '.' in expression:
+        fields = expression.split('.')
+        column = getattr(table, fields.pop(0), None)
+        if column:
+            for field in fields:
+                column = column[field]
+    else:
+        column = getattr(table, expression, None)
+    return column
+
+def cast(column, value):
     """
     将python类型值转换为SQLAlchemy类型值
 
@@ -38,11 +90,11 @@ class Filter(object):
             if len(value) == 0:
                 return query
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value[0])
+                column = cast(column, value[0])
             query = query.filter(column.in_(value))
         else:
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value)
+                column = cast(column, value)
             query = query.filter(column == value)
         return query
 
@@ -51,11 +103,11 @@ class Filter(object):
             if len(value) == 0:
                 return query
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value[0])
+                column = cast(column, value[0])
             query = query.filter(column.in_(value))
         else:
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value)
+                column = cast(column, value)
             query = query.filter(column == value)
         return query
 
@@ -64,101 +116,104 @@ class Filter(object):
             if len(value) == 0:
                 return query
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value[0])
+                column = cast(column, value[0])
             query = query.filter(column.notin_(value))
         else:
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value)
+                column = cast(column, value)
             query = query.filter(column != value)
         return query
 
     def op_eq(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column == value)
         return query
 
     def op_ne(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column != value)
         return query
 
     def op_lt(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column < value)
         return query
 
     def op_lte(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column <= value)
         return query
 
     def op_gt(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column > value)
         return query
 
     def op_gte(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column >= value)
         return query
 
     def op_like(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column.like('%%%s%%' % value))
         return query
 
     def op_starts(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column.like('%s%%' % value))
         return query
 
     def op_ends(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column.like('%%%s' % value))
         return query
 
     def op_ilike(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column.ilike('%%%s%%' % value))
         return query
 
     def op_istarts(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column.ilike('%s%%' % value))
         return query
 
     def op_iends(self, query, column, value):
         if isinstance(column, BinaryExpression):
-            column = cast_from_python_value(column, value)
+            column = cast(column, value)
         query = query.filter(column.ilike('%%%s' % value))
         return query
 
 
-class FilterNetwork(object):
+class FilterNetwork(Filter):
+    '''
+    用于PG数据库， 重写IP，CIDR的范围查询，不支持like操作
+    '''
     def op(self, query, column, value):
         if utils.is_list_type(value):
             if len(value) == 0:
                 return query
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value[0])
+                column = cast(column, value[0])
             filters = column.op("<<=")(value[0])
             for index in range(1, len(value)):
                 filters |= column.op("<<=")(value[index])
             query = query.filter(filters)
         else:
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value)
+                column = cast(column, value)
             query = query.filter(column == value)
         return query
 
@@ -167,14 +222,14 @@ class FilterNetwork(object):
             if len(value) == 0:
                 return query
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value[0])
+                column = cast(column, value[0])
             filters = column.op("<<=")(value[0])
             for index in range(1, len(value)):
                 filters |= column.op("<<=")(value[index])
             query = query.filter(filters)
         else:
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value)
+                column = cast(column, value)
             query = query.filter(column.op("<<=")(value))
         return query
 
@@ -183,13 +238,57 @@ class FilterNetwork(object):
             if len(value) == 0:
                 return query
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value[0])
+                column = cast(column, value[0])
             filters = ~column.op("<<=")(value[0])
             for index in range(1, len(value)):
                 filters &= ~column.op("<<=")(value[index])
             query = query.filter(filters)
         else:
             if isinstance(column, BinaryExpression):
-                column = cast_from_python_value(column, value)
+                column = cast(column, value)
             query = query.filter(~column.op("<<=")(value))
         return query
+
+    def op_like(self, query, column, value):
+        return query
+
+    def op_starts(self, query, column, value):
+        return query
+
+    def op_ends(self, query, column, value):
+        return query
+
+    def op_ilike(self, query, column, value):
+        return query
+
+    def op_istarts(self, query, column, value):
+        return query
+
+    def op_iends(self, query, column, value):
+        return query
+
+
+class FilterNumber(Filter):
+    '''
+    数字类型不支持like操作
+    '''
+    def op_like(self, query, column, value):
+        return query
+
+    def op_starts(self, query, column, value):
+        return query
+
+    def op_ends(self, query, column, value):
+        return query
+
+    def op_ilike(self, query, column, value):
+        return query
+
+    def op_istarts(self, query, column, value):
+        return query
+
+    def op_iends(self, query, column, value):
+        return query
+
+
+FilterDateTime = FilterNumber
