0fd92a61f0b17d39aa3591846841bad2a6f7fa03
wujj <wujj@cmrh.com>
2018-04-10 11:46:07 +0800

    新增：延迟加载装饰器，减少交叉import错误

--- talos/core/decorators.py
+++ talos/core/decorators.py
@@ -7,6 +7,7 @@
 from __future__ import absolute_import
 
 import functools
+import sys
 import threading
 
 
@@ -23,3 +24,34 @@ def singleton(cls):
         return instances[fullkey]
 
     return _singleton
+
+
+def require(mod_name, attr_name):
+    '''
+    基于描述符的延迟加载
+
+    :param mod_name: a.b.c:class
+    :param attr_name: attrubute name
+    '''
+
+    class _lazy_attribute(object):
+        def __init__(self, mod_name):
+            mods = mod_name.split(':')
+            self.mod_name = mods[0]
+            self.attr_name = mods[1] if len(mods) > 1 else None
+
+        def __get__(self, instance, owner):
+            if self.mod_name not in sys.modules:
+                __import__(self.mod_name)
+            mod = sys.modules[self.mod_name]
+            if self.attr_name:
+                return getattr(mod, self.attr_name)
+            return mod
+
+    def _require_api(cls):
+        def __require_api(*args, **kwargs):
+            setattr(cls, attr_name, _lazy_attribute(mod_name))
+            instance = cls(*args, **kwargs)
+            return instance
+        return __require_api
+    return _require_api
