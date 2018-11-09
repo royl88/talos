92abff31cc6e0d95a32610480d2e1197afb1dc81
Roy <576477085@qq.com>
2018-11-09 20:26:18 +0800

    更新：启用celery_worker的i18n支持，默认屏蔽DB支持，可以让用户自助启用

--- talos/template/server/tpl_celery_worker_py.py
+++ talos/template/server/tpl_celery_worker_py.py
@@ -11,7 +11,12 @@ from talos.server import base
 base.initialize_config(os.environ.get('${pkg_name.upper()}_CONF', '${config_file}'),
                        dir_path=os.environ.get('${pkg_name.upper()}_CONF_DIR', '${config_dir}'))
 base.initialize_logger()
+base.initialize_i18n(${pkg_name})
+# not allowed database connections by default, if you want to use db features, pls remove '#'
+# base.initialize_db()
 # import celery later, after initialize config
 from talos.common import celery
 app = celery.app
+
+
 '''
