ee9540f8a098a03c562b290bd98e5b632786ba9f
Roy <576477085@qq.com>
2018-11-20 15:35:15 +0800

    修复：校验器中的nullable & callback类型的校验失效问题

--- talos/db/crud.py
+++ talos/db/crud.py
@@ -145,13 +145,18 @@ class ColumnValidator(object):
         :returns: 验证通过返回True，失败返回错误信息
         :rtype: bool/string
         """
+        # 可以为null
         if value is None and self.nullable is True:
             return True
+        # 不需要校验
         if self.rule is None and self.rule_type is None:
             return True
+        if value is None and not self.nullable:
+            result = _('not nullable')
         elif self.rule_type == 'callback':
             # 如果是用户指定callback，直接调用
-            if self.rule(value) is True:
+            result = self.rule(value)
+            if result is True:
                 return True
         else:
             # 否则使用通用validator
