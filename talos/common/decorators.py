a3c157fb1d230ff4d7c3bfe7466a8dbd8bb09f00
Roy <576477085@qq.com>
2019-03-13 14:50:45 +0800

    更新：limit组件支持装饰器定义限制策略

--- talos/common/decorators.py
+++ talos/common/decorators.py
@@ -15,13 +15,15 @@ LIMITEDS = {}
 LIMITED_EXEMPT = {}
 
 
-def limit(limit_value, key_function=None, scope=None, per_method=True, msg_fmt=None):
+def limit(limit_value, key_function=None, scope=None, per_method=True, strategy=None, message=None, hit_func=None):
     """
     用于装饰一个controller表示其受限于此调用频率
     :param limit_value: limits的调用频率字符串或一个能返回限制器的函数.
     :param function key_func: 一个返回唯一标识字符串的函数，用于标识一个limiter,比如远端IP.
     :param function scope: 调用频率限制范围的命名空间.
-    :param msg_fmt: 错误提示信息可接受3个格式化（limit，remaining，reset）内容.
+    :param strategy: 频率限制算法策略.
+    :param message: 错误提示信息可接受3个格式化（limit，remaining，reset）内容.
+    :param hit_func: 使用自定义hit计算，并非每次访问都触发hit，而由用户自定定义
     """
 
     def _inner(fn):
@@ -30,7 +32,8 @@ def limit(limit_value, key_function=None, scope=None, per_method=True, msg_fmt=N
         def __inner(*args, **kwargs):
             instance = fn(*args, **kwargs)
             LIMITEDS.setdefault(instance, []).append(
-                LimitWrapper(limit_value, key_function, scope, per_method=per_method, msg_fmt=msg_fmt)
+                LimitWrapper(limit_value, key_function, scope, per_method=per_method,
+                             strategy=strategy, message=message, hit_func=hit_func)
             )
             return instance
 
--- tests/test_limit.py
+++ tests/test_limit.py
@@ -10,6 +10,12 @@ from talos.middlewares import limiter
 from talos.common import decorators as deco
 
 
+class ControllerGlobal(object):
+
+    def on_get(self, req, resp):
+        pass
+
+
 @deco.limit('1/second')
 class ControllerPerMethod(object):
 
@@ -30,7 +36,7 @@ class ControllerPerCtl(object):
         pass
 
 
-@deco.limit('1/second', msg_fmt='ooh, max access: %(limit)s time reach, you can retry after %(reset)s seconds')
+@deco.limit('1/second', message='ooh, max access: %(limit)s time reach, you can retry after %(reset)s seconds')
 class ControllerUserMsg(object):
 
     def on_get(self, req, resp):
@@ -45,7 +51,33 @@ def key_extract(req):
 @deco.limit('1/second', key_function=key_extract)
 class ControllerUserKeyFunc(object):
 
+    def on_post(self, req, resp):
+        pass
+
+
+@deco.limit('2/5second', strategy='fixed-window')
+class ControllerFixedWindow(object):
+
     def on_get(self, req, resp):
+        pass
+
+
+@deco.limit('2/5second', strategy='moving-window')
+class ControllerMovingWindow(object):
+
+    def on_get(self, req, resp):
+        pass
+
+
+def hit_only_test(resource, request):
+    if request.media['name'] == 'testhit':
+        return True
+
+
+@deco.limit('3/3second', hit_func=hit_only_test)
+class ControllerUserHitFunc(object):
+
+    def on_post(self, req, resp):
         pass
 
 
@@ -77,12 +109,17 @@ def error_serializer(req, resp, exception):
         resp.content_type = preferred
 
 
-app = falcon.API(middleware=[limiter.Limiter(True, [], 'fixed-window',
-                                             'memory://', False, 'X-RESET', 'X-REMAIN', 'X-LIMIT')])
+app = falcon.API(middleware=[limiter.Limiter(True, '10/second', 'fixed-window',
+                                             'memory://', False, None, 'X-RESET', 'X-REMAIN', 'X-LIMIT')])
+app.add_route('/test', ControllerGlobal())
 app.add_route('/permethod', ControllerPerMethod())
 app.add_route('/perctl', ControllerPerCtl())
 app.add_route('/usermsg', ControllerUserMsg())
 app.add_route('/userkeyfunc', ControllerUserKeyFunc())
+app.add_route('/fixedwindow', ControllerFixedWindow())
+app.add_route('/movingwindow', ControllerMovingWindow())
+app.add_route('/userhitfunc', ControllerUserHitFunc())
+
 app.add_error_handler(Exception, error_http_exception)
 app.set_error_serializer(error_serializer)
 client = testing.TestClient(app)
@@ -91,30 +128,52 @@ client = testing.TestClient(app)
 # httpd.serve_forever()
 
 
+def test_global_limit():
+    resp = client.simulate_get('/test')
+    resp = client.simulate_get('/test')
+    resp = client.simulate_get('/test')
+    resp = client.simulate_get('/test')
+    resp = client.simulate_get('/test')
+    resp = client.simulate_get('/test')
+    resp = client.simulate_get('/test')
+    resp = client.simulate_get('/test')
+    resp = client.simulate_get('/test')
+    resp = client.simulate_get('/test')
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_get('/test')
+    assert resp.status == falcon.HTTP_429
+    resp = client.simulate_get('/test')
+    assert resp.status == falcon.HTTP_429
+
+
 def test_permethod_ok():
     time.sleep(1.1)
-    client.simulate_get('/permethod')
-    client.simulate_post('/permethod')
+    resp = client.simulate_get('/permethod')
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/permethod')
+    assert resp.status == falcon.HTTP_200
 
 
 def test_permethod_limit():
     time.sleep(1.1)
-    client.simulate_get('/permethod')
-    client.simulate_post('/permethod')
+    resp = client.simulate_get('/permethod')
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/permethod')
+    assert resp.status == falcon.HTTP_200
     resp = client.simulate_get('/permethod')
     assert resp.status == falcon.HTTP_429
 
 
 def test_perctl_limit():
-    time.sleep(1.1)
-    client.simulate_get('/perctl')
+    resp = client.simulate_get('/perctl')
+    assert resp.status == falcon.HTTP_200
     resp = client.simulate_post('/perctl')
     assert resp.status == falcon.HTTP_429
 
 
 def test_usermsg_limit():
-    time.sleep(1.1)
-    client.simulate_get('/usermsg')
+    resp = client.simulate_get('/usermsg')
+    assert resp.status == falcon.HTTP_200
     resp = client.simulate_get('/usermsg')
     assert resp.status == falcon.HTTP_429
     assert resp.json['description'].startswith(u'ooh, max access')
@@ -122,15 +181,100 @@ def test_usermsg_limit():
 
 def test_userkeyfunc_ok():
     time.sleep(1.1)
-    client.simulate_post('/userkeyfunc', json={'name': 'test1'})
-    client.simulate_post('/userkeyfunc', json={'name': 'test2'})
-    client.simulate_post('/userkeyfunc', json={'name': 'test3'})
-    client.simulate_post('/userkeyfunc', json={'name': 'test4'})
-    client.simulate_post('/userkeyfunc', json={'name': 'test5'})
+    resp = client.simulate_post('/userkeyfunc', json={'name': 'test1'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userkeyfunc', json={'name': 'test2'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userkeyfunc', json={'name': 'test3'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userkeyfunc', json={'name': 'test4'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userkeyfunc', json={'name': 'test5'})
+    assert resp.status == falcon.HTTP_200
 
 
 def test_userkeyfunc_limit():
     time.sleep(1.1)
-    client.simulate_post('/userkeyfunc', json={'name': 'test123'})
     resp = client.simulate_post('/userkeyfunc', json={'name': 'test123'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userkeyfunc', json={'name': 'test123'})
+    assert resp.status == falcon.HTTP_429
+
+
+def test_fixed_window():
+    resp = client.simulate_get('/fixedwindow')
+    assert resp.status == falcon.HTTP_200
+    time.sleep(2)
+    resp = client.simulate_get('/fixedwindow')
+    assert resp.status == falcon.HTTP_200
+    time.sleep(1)
+    resp = client.simulate_get('/fixedwindow')
+    assert resp.status == falcon.HTTP_429
+    time.sleep(1)
+    resp = client.simulate_get('/fixedwindow')
+    assert resp.status == falcon.HTTP_429
+    time.sleep(1)
+    resp = client.simulate_get('/fixedwindow')
+    assert resp.status == falcon.HTTP_200
+    time.sleep(1)
+    resp = client.simulate_get('/fixedwindow')
+    assert resp.status == falcon.HTTP_200
+    time.sleep(1)
+    resp = client.simulate_get('/fixedwindow')
+    assert resp.status == falcon.HTTP_429
+    time.sleep(1)
+    resp = client.simulate_get('/fixedwindow')
+    assert resp.status == falcon.HTTP_429
+
+
+def test_moving_window():
+    resp = client.simulate_get('/movingwindow')
+    assert resp.status == falcon.HTTP_200
+    time.sleep(2)
+    resp = client.simulate_get('/movingwindow')
+    assert resp.status == falcon.HTTP_200
+    time.sleep(1)
+    resp = client.simulate_get('/movingwindow')
+    assert resp.status == falcon.HTTP_429
+    time.sleep(1)
+    resp = client.simulate_get('/movingwindow')
+    assert resp.status == falcon.HTTP_429
+    time.sleep(1)
+    resp = client.simulate_get('/movingwindow')
+    assert resp.status == falcon.HTTP_200
+    time.sleep(1)
+    resp = client.simulate_get('/movingwindow')
+    assert resp.status == falcon.HTTP_429
+    time.sleep(1)
+    resp = client.simulate_get('/movingwindow')
+    assert resp.status == falcon.HTTP_200
+    time.sleep(1)
+    resp = client.simulate_get('/movingwindow')
+    assert resp.status == falcon.HTTP_429
+
+
+def test_user_hit_func():
+    resp = client.simulate_post('/userhitfunc', json={'name': 'test1'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'test2'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'test3'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'test4'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'test5'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'test6'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'test7'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'testhit'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'testhit'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'testhit'})
+    assert resp.status == falcon.HTTP_200
+    resp = client.simulate_post('/userhitfunc', json={'name': 'testhit'})
+    assert resp.status == falcon.HTTP_429
+    resp = client.simulate_post('/userhitfunc', json={'name': 'testhit'})
     assert resp.status == falcon.HTTP_429
