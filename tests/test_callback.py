59e3a6d34071df4af6573eaa1deb60e76f7b77b6
Roy <576477085@qq.com>
2019-09-10 22:40:28 +0800

    更新：callback单元测试运行问题

--- tests/test_callback.py
+++ tests/test_callback.py
@@ -3,14 +3,14 @@
 import time
 import logging
 import pytest
-import multiprocessing
+import multiprocessing as concurrent
 from wsgiref.simple_server import make_server
 
+from talos.server import base
 from talos.core import config
 from talos.core import exceptions
 
 from tests.apps.cats import callback
-
 
 LOG = logging.getLogger(__name__)
 CONF = config.CONF
@@ -18,12 +18,10 @@ CONF = config.CONF
 
 def proc(lock):
     try:
-        from talos.server import base
+        
         application = base.initialize_server('test', './tests/unittest.conf')
-
         bind_addr = CONF.server.bind
         port = CONF.server.port
-        print("try 'pip install gevent' to boost simple server")
         print("Serving on %s:%d..." % (bind_addr, port))
         httpd = make_server(bind_addr, port, application)
     finally:
@@ -33,7 +31,7 @@ def proc(lock):
 
 def start_server(lock):
     lock.acquire()
-    p = multiprocessing.Process(target=proc, args=(lock, ))
+    p = concurrent.Process(target=proc, args=(lock,))
     p.start()
     return p
 
@@ -44,30 +42,32 @@ def test_add_local():
 
 
 def test_add_remmote():
-    l = multiprocessing.Lock()
+    l = concurrent.Lock()
     p = start_server(l)
     time.sleep(1.0)
-    with l:
+    
+    try:
+        l.acquire(timeout=15)
         ret = callback.add.remote(None, x=3, y=4)
         assert ret['result'] == 7
-    # p.join()
+    finally:
+        l.release()
+    p.join()
 
 
 def test_timeout_baseurl():
-    l = multiprocessing.Lock()
-    p = start_server(l)
-    time.sleep(1.0)
     with pytest.raises(exceptions.CallBackError):
-        with l:
-            ret = callback.timeout.baseurl('http://1.2.3.4:9000').context(timeout=3).remote(None)
-    # p.join()
+        ret = callback.timeout.baseurl('http://1.2.3.4:9000').context(timeout=3.0).remote(None)
 
 
 def test_timeout_context():
-    l = multiprocessing.Lock()
+    l = concurrent.Lock()
     p = start_server(l)
     time.sleep(1.0)
     with pytest.raises(exceptions.CallBackError):
-        with l:
+        try:
+            l.acquire(timeout=15)
             ret = callback.timeout.context(timeout=1).remote(None)
-    # p.join()
+        finally:
+            l.release()
+    p.join()
