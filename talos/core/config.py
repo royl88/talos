e28ef7e0c10cc860b217ef8c64236a3413b72581
Roy <576477085@qq.com>
2019-11-22 18:33:28 +0800

    更新：优化config的get访问效率

--- talos/core/config.py
+++ talos/core/config.py
@@ -167,6 +167,13 @@ class Config(Mapping):
         "D.values() -> list of D's values"
         return [self._opts[key] for key in self._opts]
 
+    def get(self, key, default=None):
+        'D.get(k[,d]) -> D[k] if k in D, else d.  d defaults to None.'
+        try:
+            return self._opts[key]
+        except KeyError:
+            return default
+
     def to_dict(self):
         return self._opts
 
