b501fdcffa6b902447c7609ccf966f9b1f5e2197
Roy <576477085@qq.com>
2020-03-21 18:39:51 +0800

    更新：i18n支持多种语言加载和切换

--- talos/core/i18n.py
+++ talos/core/i18n.py
@@ -8,7 +8,7 @@ from __future__ import absolute_import
 
 import gettext
 import logging
-
+from talos.core import utils
 
 LOG = logging.getLogger(__name__)
 
@@ -21,19 +21,73 @@ class Translator():
     """
 
     def __init__(self):
-        self.translation = None
+        self.default_language = None
+        self._translation_maps = {}
 
     def __call__(self, value):
-        if self.translation:
-            return self.translation.gettext(value)
+        if self._translation_maps and self.default_language:
+            return self._translation_maps[self.default_language].gettext(value)
         else:
             return value
 
     def setup(self, app, locales, lang):
-        try:
-            self.translation = gettext.translation(app, locales, [lang])
-        except IOError:
-            LOG.warning('language(%s) files not found, no translation will be used', lang)
+        # 清除成员信息以支持多次初始化
+        self.default_language = None
+        self._translation_maps = {}
+        if utils.is_list_type(lang):
+            if len(lang) == 0:
+                LOG.warning('language(%s) files not found, no translation will be used', lang)
+            for l in lang:
+                find_mo = gettext.find("makaira",
+                                    localedir=locales,
+                                    languages=[l])
+                if find_mo:
+                    # 加载所有指定语言 & 设置第一个有效mo文件的语言为默认语言
+                    with open(find_mo, 'rb') as f:
+                        self._translation_maps[l] = gettext.GNUTranslations(f)
+                    if self.default_language is None:
+                        self.default_language = l
+        else:
+            self.default_language = lang
+            try:
+                self._translation_maps[lang] = gettext.translation(app, locales, [lang])
+            except IOError:
+                LOG.warning('language(%s) files not found, no translation will be used', lang)
+
+    def change(self, lang):
+        # 更改默认翻译语言
+        if lang in self.available_languages:
+            self.default_language = lang
+            return True
+        else:
+            return False
+
+    @property
+    def available_languages(self):
+        return list(self._translation_maps.keys())
+    
+    def client_prefers(self, accept, supported=None):
+
+        def parse_language_range(language):
+            lang, params = '', {}
+            language_opts = language.split(';')
+            language_opts = [i.strip() for i in language_opts if i.strip()]
+            if len(language_opts) > 1:
+                lang = language_opts[0]
+                params['q'] = float(language_opts[1].lower().split('=')[1])
+            else:
+                lang = language_opts[0]
+                params['q'] = 1.0
+            return (lang, params)
+
+        split_languages = [i.strip() for i in accept.split(',') if i.strip()]
+        split_languages = [parse_language_range(i) for i in split_languages]
+        split_languages = sorted(split_languages, key=lambda x: x[1]['q'], reverse=True)
+        supported = supported or self.available_languages
+        for lang in split_languages:
+            if lang[0] in supported:
+                return lang
+        return None
 
 
 _ = Translator()
